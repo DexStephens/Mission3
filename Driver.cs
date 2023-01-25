f7a978dd1fcad397fb54c494da84cd8dd1825826
Rachel Daniel <racheldaniel328@gmail.com>
2023-01-25 13:11:14 -0700

    added comments to driver page

--- Driver.cs
+++ Driver.cs
@@ -10,14 +10,16 @@ namespace Mission3
     {
 		public void StartGame()
 		{
+			//initialize game board with numbers 1 through 9 and set counter
 			Console.WriteLine("Welcome to the Game!!!");
 			char[] gameBoard = new char[] {'1', '2', '3', '4', '5', '6','7','8','9',};
 			int counter = 0;
 
+			//create instance of board master
 			BoardMaster bm = new BoardMaster();
 
 			
-
+			//Enter Game loop
 			while (counter < 9)
             {
 				BoardMaster.PrintBoard(gameBoard);
@@ -25,6 +27,7 @@ namespace Mission3
 				int turnPlayer1 = 0;
 				bool valid = false;
 
+				//Enter loop for player 1. Valid is initalized to false, and checks that inputs are valid and square hasn't been used
 				while (valid == false)
                 {
 					try
@@ -34,7 +37,9 @@ namespace Mission3
 						{
 							Console.WriteLine("Invalid input or square already used, try again");
 						}
+						//if conditions are met, update gameboard and set valid to true
 						else
+
 						{
 							gameBoard[turnPlayer1 - 1] = 'x';
 							counter++;
@@ -46,6 +51,8 @@ namespace Mission3
 						Console.WriteLine("Invalid input or square already used, try again");
 					}
                 }
+
+				// check for a cat's game or a winner
 				char? result = BoardMaster.IsWinner(gameBoard);
 				if (result != null) {
 					if (result == 'C')
@@ -60,6 +67,7 @@ namespace Mission3
 					break;
 				}
 
+				//replicate previous logic for player 2
 				BoardMaster.PrintBoard(gameBoard);
 				Console.WriteLine("Player 2: Choose your square");
 				int turnPlayer2 = 0;
