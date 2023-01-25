5173da2d50f8a47efc8d0dbb541bed16cd3ef946
Rachel Daniel <racheldaniel328@gmail.com>
2023-01-25 09:34:03 -0700

    added comments to board master

--- BoardMaster.cs
+++ BoardMaster.cs
@@ -9,19 +9,22 @@ namespace Mission3
 {
     public class BoardMaster
     {
+        // create the tic tac toe board formatting
         public static void PrintBoard(char[] board)
         {
-            Console.WriteLine($"  {board[0]}  | {board[1]}   | {board[2]}  ");
+            Console.WriteLine($"  {board[0]}  |  {board[1]}  |  {board[2]} ");
             Console.WriteLine($"_____|_____|_____");
             Console.WriteLine($"  {board[3]}  |  {board[4]}  |  {board[5]} ");
             Console.WriteLine($"_____|_____|_____");
             Console.WriteLine($"  {board[6]}  |  {board[7]}  |  {board[8]} ");
+            Console.WriteLine(""); // blank line so output looks better
         }
 
+        // Calculate the winner or determine if result is cat's game
         public static char? IsWinner(char[] board)
         {
             List<char> numList = new() { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-            //Check for horizontal winners
+            // Check for horizontal winners & output letter of winner
             if (board[0] == board[1] && board[1] == board[2])
             {
                 return board[0];
@@ -34,7 +37,7 @@ namespace Mission3
             {
                 return board[6];
             }
-            //Check for vertical winners
+            // Check for vertical winners & output letter of winner
             if (board[0] == board[3] && board[3] == board[6])
             {
                 return board[0];
@@ -47,7 +50,7 @@ namespace Mission3
             {
                 return board[2];
             }
-            //Check for diagonal winners
+            // Check for diagonal winners & output letter of winner
             if (board[0] == board[4] && board[4] == board[8])
             {
                 return board[0];
@@ -56,6 +59,7 @@ namespace Mission3
             {
                 return board[2];
             }
+            // Return C if cat game
             if (Array.Find(board, x => numList.Contains(x)) == '\0')
             {
                 return 'C';
