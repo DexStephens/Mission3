8384ae2a15f9f2945dada5ff7dd87fe3597ffb46
Jake Evans <ejake370@byu.edu>
2023-01-25 08:40:21 -0700

    Created Driver Class

---
+++ Driver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission3
+{
+    public class Driver
+    {
+		public void StartGame()
+		{
+			Console.WriteLine("Welcome to the Game!!!");
+			char[] gameBoard = new char[] {'1', '2', '3', '4', '5', '6','7','8','9',};
+			int counter = 0;
+
+			BoardMaster bm = new BoardMaster();
+
+			BoardMaster.PrintBoard(gameBoard);
+
+
+		}
+	}
+}
+
